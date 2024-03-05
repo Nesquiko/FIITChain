bf00bfb95579c5af98dd0201fe54a1974da1bc85
Nesquiko <lukas.castven@gmail.com>
2024-03-05 20:20:46 +0100

    init MaxFeeHandler struct

--- fiitcoin/src/handler.rs
+++ fiitcoin/src/handler.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use rsa::{pkcs1v15::Signature, signature::Verifier};
+use rsa::{pkcs1v15::Signature, signature::Verifier, RsaPublicKey};
 
 use crate::{
     tx::{raw_tx_from_one_input, Input, Tx},
@@ -17,94 +17,18 @@ pub trait TxHandler<'a> {
     /// transactions can reference same output.
     fn handle(&mut self, possible_txs: Vec<&'a Tx>) -> Vec<&'a Tx>;
 
+    fn pool(&self) -> &UTXOPool;
+
     /// Checks if:
     ///     1. All UTXO inputs are in pool
     ///     2. Signatures on inputs are valid
     ///     3. No UTXO is used more than once
     ///     4. Sum of outputs is not negative
     ///     5. Sum of inputs >= Sum of outputs
-    fn is_tx_valid(&self, tx: &Tx) -> bool;
-}
-
-pub struct Handler {
-    pool: UTXOPool,
-}
-
-impl Handler {
-    pub fn new(pool: UTXOPool) -> Self {
-        Self { pool }
-    }
-
-    fn input_to_utxo(&self, input: &Input) -> UTXO {
-        UTXO::new(input.output_tx_hash(), input.output_idx())
-    }
-
-    fn apply_tx(&mut self, tx: &Tx) {
-        for input in tx.inputs().iter() {
-            self.pool.remove_utxo(&self.input_to_utxo(input));
-        }
-        for (i, output) in tx.outputs().iter().enumerate() {
-            let utxo = UTXO::new(tx.hash(), i.try_into().unwrap());
-            // clone is here necessary, because I want to return the tx back to
-            // caller, so I can't consume it
-            self.pool.add_utxo(utxo, output.clone())
-        }
-    }
-
-    fn is_input_in_pool(&self, input: &Input) -> bool {
-        self.pool.contains(&self.input_to_utxo(input))
-    }
-
-    /// Filters independent txs from dependent ones, applies them and returns both sets
-    fn handle_independent<'a>(&mut self, txs: Vec<&'a Tx>) -> (Vec<&'a Tx>, Vec<&'a Tx>) {
-        let mut handled = vec![];
-        let mut dependent = vec![];
-        let tx_set: HashSet<[u8; 32]> = txs.iter().map(|&tx| tx.hash()).collect();
-
-        for &tx in txs.iter() {
-            if tx.inputs().iter().all(|i| self.is_input_in_pool(i)) {
-                // tx is only dependent on outputs in pool
-                if self.is_tx_valid(tx) {
-                    self.apply_tx(tx);
-                    handled.push(tx);
-                }
-            } else if tx
-                .inputs()
-                .iter()
-                .any(|i| tx_set.contains(&i.output_tx_hash()))
-            {
-                // tx is dependent on some outputs from this batch
-                dependent.push(tx)
-            }
-        }
-
-        (handled, dependent)
-    }
-
-    // fn balance_of(&self, )
-}
-
-impl<'a> TxHandler<'a> for Handler {
-    fn handle(&mut self, possible_txs: Vec<&'a Tx>) -> Vec<&'a Tx> {
-        let mut handled: Vec<&'a Tx> = vec![];
-        let mut to_handle = possible_txs;
-
-        loop {
-            let (independent, dependent) = self.handle_independent(to_handle);
-            handled.extend(independent);
-            if dependent.is_empty() {
-                break;
-            }
-            to_handle = dependent;
-        }
-
-        handled
-    }
-
     fn is_tx_valid(&self, tx: &Tx) -> bool {
         let mut in_sum = 0;
         for (i, input) in tx.inputs().iter().enumerate() {
-            let output = match self.pool.utxo_output(&self.input_to_utxo(input)) {
+            let output = match self.pool().utxo_output(&input_to_utxo(input)) {
                 Some(out) => out,
                 None => {
                     log::debug!(
@@ -156,4 +80,104 @@ impl<'a> TxHandler<'a> for Handler {
 
         out_sum > 0 && in_sum >= out_sum
     }
+}
+
+pub struct Handler {
+    pool: UTXOPool,
+}
+
+impl Handler {
+    pub fn new(pool: UTXOPool) -> Self {
+        Self { pool }
+    }
+
+    pub fn balance_of(&self, pub_key: &RsaPublicKey) -> u64 {
+        self.pool
+            .utxos_of(pub_key)
+            .iter()
+            .map(|output| output.value() as u64)
+            .sum()
+    }
+
+    fn apply_tx(&mut self, tx: &Tx) {
+        for input in tx.inputs().iter() {
+            self.pool.remove_utxo(&input_to_utxo(input));
+        }
+        for (i, output) in tx.outputs().iter().enumerate() {
+            let utxo = UTXO::new(tx.hash(), i.try_into().unwrap());
+            // clone is here necessary, because I want to return the tx back to
+            // caller, so I can't consume it
+            self.pool.add_utxo(utxo, output.clone())
+        }
+    }
+
+    fn is_input_in_pool(&self, input: &Input) -> bool {
+        self.pool.contains(&input_to_utxo(input))
+    }
+
+    /// Filters independent txs from dependent ones, applies them and returns both sets
+    fn handle_independent<'a>(&mut self, txs: Vec<&'a Tx>) -> (Vec<&'a Tx>, Vec<&'a Tx>) {
+        let mut handled = vec![];
+        let mut dependent = vec![];
+        let tx_set: HashSet<[u8; 32]> = txs.iter().map(|&tx| tx.hash()).collect();
+
+        for &tx in txs.iter() {
+            if tx.inputs().iter().all(|i| self.is_input_in_pool(i)) {
+                // tx is only dependent on outputs in pool
+                if self.is_tx_valid(tx) {
+                    self.apply_tx(tx);
+                    handled.push(tx);
+                }
+            } else if tx
+                .inputs()
+                .iter()
+                .any(|i| tx_set.contains(&i.output_tx_hash()))
+            {
+                // tx is dependent on some outputs from this batch
+                dependent.push(tx)
+            }
+        }
+
+        (handled, dependent)
+    }
+}
+
+impl<'a> TxHandler<'a> for Handler {
+    fn handle(&mut self, possible_txs: Vec<&'a Tx>) -> Vec<&'a Tx> {
+        let mut handled: Vec<&'a Tx> = vec![];
+        let mut to_handle = possible_txs;
+
+        loop {
+            let (independent, dependent) = self.handle_independent(to_handle);
+            handled.extend(independent);
+            if dependent.is_empty() {
+                break;
+            }
+            to_handle = dependent;
+        }
+
+        handled
+    }
+
+    fn pool(&self) -> &UTXOPool {
+        &self.pool
+    }
+}
+
+pub struct MaxFeeHandler {
+    pool: UTXOPool,
+}
+
+impl<'a> TxHandler<'a> for MaxFeeHandler {
+    fn handle(&mut self, possible_txs: Vec<&'a Tx>) -> Vec<&'a Tx> {
+        todo!()
+    }
+
+    fn pool(&self) -> &UTXOPool {
+        &self.pool
+    }
+}
+
+fn input_to_utxo(input: &Input) -> UTXO {
+    UTXO::new(input.output_tx_hash(), input.output_idx())
 }
